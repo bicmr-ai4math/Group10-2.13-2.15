066dd4c811b8474f1d1fb8bf2a633f52bfda176e
yhtq <1414672068@qq.com>
2024-01-21 23:02:12 +0800

    problem_a' finish

--- Tutorial/pro213.lean
+++ Tutorial/pro213.lean
@@ -133,6 +133,9 @@ theorem problem_a (a : Fin m → ℝ) (X : Matrix (Fin m) (Fin n) ℝ) (b : Fin 
       sorry
     sorry
 
+-- 计算 a^T Xb 的导数，大致思路为容易验证导数 D 应当满足 D . V = a^T V b，取 D = a^T b ，分别验证两个等式即可
+-- 主要困难在于需要用开集的条件规约出tendsTo 内部的 t != 0，
+-- 这里通过用 eventuallyEq_nhdsWithin_of_eqOn 证明引理引导 apply tendsto_congr' 自动匹配解决
 theorem problem_a' (a : Fin m → ℝ) (X : Matrix (Fin m) (Fin n) ℝ) (b : Fin n → ℝ)
   : HasGateauxDerivAt m n (f_aXb a b) X (vecMulVec a b) := by
     simp [HasGateauxDerivAt]
