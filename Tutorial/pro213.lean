caf9e0b478433f84fe9babb76ed0e7f57b5435aa
catmeow123456 <eden@stu.pku.edu.cn>
2024-01-22 21:45:11 +0800

    pro213 first try

--- Tutorial/pro213.lean
+++ Tutorial/pro213.lean
@@ -20,6 +20,8 @@ import Mathlib.Data.Fin.Tuple.Reflection
 import Mathlib.LinearAlgebra.Matrix.PosDef
 import Mathlib.LinearAlgebra.Matrix.Adjugate
 import Mathlib.LinearAlgebra.FiniteDimensional
+import Mathlib.LinearAlgebra.Matrix.Block
+
 
 -- 2.13
 #check Filter.Tendsto
@@ -77,18 +79,42 @@ theorem final_conclusion (n : Nat) (a b: Matrix (Fin n) (Fin n) ℝ ) :
   simp at hc
   exact hc
 
+
 -- define of upper triangle matrix
-def is_upper_triangle (n : Nat) (A : Matrix (Fin n) (Fin n) ℝ) : Prop :=
-  ∀ i j : Fin n, j < i → A i j = 0
+def is_upper_triangle {n : Nat} (A : Matrix (Fin n) (Fin n) ℝ) : Prop :=
+  Matrix.BlockTriangular A id
+
+theorem is_upper_triangle.smul {n : Nat} {A : Matrix (Fin n) (Fin n) ℝ} {c : ℝ}
+  (hA : is_upper_triangle A) : is_upper_triangle (c • A) := by
+    simp [is_upper_triangle, BlockTriangular] at *
+    intro _ _ hij
+    exact Or.inr (hA hij)
+
+theorem is_upper_triangle.add {n : Nat} {A B : Matrix (Fin n) (Fin n) ℝ}
+    (hA : is_upper_triangle A) (hB : is_upper_triangle B): is_upper_triangle (A + B) := by
+  simp [is_upper_triangle] at *
+  exact Matrix.BlockTriangular.add hA hB
+
+theorem is_upper_triangle.one {n : Nat} : is_upper_triangle (1 : Matrix (Fin n) (Fin n) ℝ) := by
+  simp [is_upper_triangle]
+  exact Matrix.blockTriangular_one
+
+theorem upper_triangle_det {n : Nat} {A : Matrix (Fin n) (Fin n) ℝ} (h : is_upper_triangle A) :
+  det A = ∏ i : Fin n, A i i := by
+  simp [is_upper_triangle] at h
+  exact (Matrix.det_of_upperTriangular h)
+
+-- Matrix.det_of_upperTriangular
 
 -- define of orthogonal matrix
-def Orthogonal_Matrix (n : Nat) (A : Matrix (Fin n) (Fin n) ℝ ) : Prop :=
-  A * Aᵀ = 1
+def Orthogonal_Matrix {n : Nat} (A : Matrix (Fin n) (Fin n) ℝ ) : Prop :=
+  Aᵀ * A = 1
 
 -- schur decomposition theorem
 theorem schur_decomposition (n: Nat) (A : Matrix (Fin n) (Fin n) ℝ) :
-  ∃ U R, Orthogonal_Matrix n U ∧ is_upper_triangle n R ∧ A = Uᵀ * R * U := by
+  ∃ U R, Orthogonal_Matrix U ∧ is_upper_triangle R ∧ A = Uᵀ * R * U := by
   sorry
+
 
 open GateauxDeriv
 -- 2.13(a)
@@ -153,7 +179,68 @@ noncomputable
 def f_lndet : Matrix (Fin n) (Fin n) ℝ → ℝ :=
   fun X => Real.log X.det
 
-theorem problem_c (X : Matrix (Fin n) (Fin n) ℝ)
-  (h : ∃ f', HasGateauxDerivAt (f_lndet) X f'):
-  GateauxDeriv (f_lndet) X h = (X⁻¹)ᵀ  :=
-  sorry
+theorem problem_c (X : Matrix (Fin n) (Fin n) ℝ) (h : X.det > 0):
+  HasGateauxDerivAt (f_lndet) X (X⁻¹)ᵀ := by
+    simp [HasGateauxDerivAt]
+    simp [f_lndet]
+    intro V
+    have h_isunitdet : IsUnit X.det := by
+      simp [IsUnit]
+      linarith [h]
+    have h (t : ℝ) : X + t • V = X * ( 1 + t • X⁻¹ * V) := by
+      simp [Matrix.mul_add]
+      simp [← Matrix.mul_assoc]
+      rw [mul_nonsing_inv]
+      simp [Matrix.one_mul]
+      assumption
+    rw [Metric.tendsto_nhdsWithin_nhds]
+    -- ∀  ε > 0, ∃ 0 < δ < f(ε), ....
+    -- need to construct f(ε)
+    simp [h]
+    have none_zero_1 : det ( X ) ≠  0 := by
+      linarith [h]
+    have none_zero_2 (x : ℝ) : det ( 1 + x • (X⁻¹ * V) ) ≠  0 := by
+      sorry
+    simp [Real.log_mul, none_zero_1, none_zero_2]
+    let ⟨Q, R, h3a, h3b, h3c⟩  := schur_decomposition n (X⁻¹ * V)
+    rw [h3c]
+    have h4 : 1 = Qᵀ * Q := by
+      dsimp [Orthogonal_Matrix] at h3a
+      exact symm h3a
+    rw [h4]
+    have h5 : Q = 1 * Q := by
+      rw [Matrix.one_mul]
+    nth_rewrite 2[h5]
+    rw [← Matrix.mul_assoc]
+    have h6: Qᵀ * R * Q = Qᵀ * (R * Q) :=by
+      rw [Matrix.mul_assoc]
+    rw [h6]
+    have h7 (t : ℝ ) : t • (Qᵀ * (R * Q)) = Qᵀ * (t • R) * Q :=by
+      rw [← Matrix.mul_assoc]
+      rw[Matrix.mul_smul]
+      simp
+    simp only [h7]
+    simp only [← Matrix.add_mul]
+    simp only [← Matrix.mul_add]
+    simp
+    have : 1 = Q.det * Q.det := by
+      have hh := congrArg Matrix.det h4
+      simp at hh
+      assumption
+    simp only [mul_comm]
+    simp only [← mul_assoc]
+    simp [← this]
+    have h8 (x : ℝ) : is_upper_triangle (1 + x • R) := by
+      apply is_upper_triangle.add
+      apply is_upper_triangle.one
+      apply is_upper_triangle.smul
+      exact h3b
+    simp only [upper_triangle_det, h8]
+    have h9 (x : ℝ) (i : Fin n): (1 + x • R) i i ≠ 0 := by
+      sorry
+    sorry
+
+#check Matrix.one_mul
+#check IsUnit
+
+#check Finset
