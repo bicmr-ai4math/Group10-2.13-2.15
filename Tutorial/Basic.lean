f4ca53da3a3bb1446fa560665db40e74ff9e6240
XX248 <3348669200@qq.com>
2024-01-23 21:24:47 +0800

    I've modified
    1111

--- Tutorial/Basic.lean
+++ Tutorial/Basic.lean
@@ -8,7 +8,7 @@ import Mathlib.Order.Filter.Basic
 import Mathlib.Topology.Basic
 -- import Mathlib.Topology.Instances.Matrix
 -- import Mathlib.LinearAlgebra.Matrix.NonsingularInverse
--- import Mathlib.Analysis.SpecialFunctions.Exp
+import Mathlib.Analysis.SpecialFunctions.Log
 -- import Mathlib.Data.Nat.Factorization.Basic
 -- import Mathlib.LinearAlgebra.Matrix.PosDef
 -- import Mathlib.LinearAlgebra.Matrix.Adjugate
